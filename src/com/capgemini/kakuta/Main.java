9f49eca7688083113f0e505fdaea1cfb7298a422
Kamil Kuta <kamil.kuta@capgemini.com>
2024-06-10 11:27:58 +0200

    git init

---
+++ src/com/capgemini/kakuta/Main.java
@@ -0,0 +1,7 @@
+package com.capgemini.kakuta;
+
+public class Main {
+
+    public static void main(String[] args) {
+    }
+}
---
+++ src/com/capgemini/kakuta/ReturnValues.java
@@ -0,0 +1,5 @@
+package com.capgemini.kakuta;
+
+public class ReturnValues {
+
+}
