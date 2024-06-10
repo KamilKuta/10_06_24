1a8bf8e45cc46603481f84e483cbea2a8eda46e1
KamilKuta <52802686+KamilKuta@users.noreply.github.com>
2024-06-10 16:56:10 +0200

    Dice jack implementation (#1)
    * implementation for DiceJack game
    
    ---------
    
    Authored-by: Kamil Kuta <kamilkuta97@gmail.com>

---
+++ src/com/capgemini/kakuta/DiceJack/RollTheDice.java
@@ -0,0 +1,29 @@
+package com.capgemini.kakuta.DiceJack;
+
+import java.util.Random;
+
+public class RollTheDice {
+    static Random randomNumber;
+    static int number01;
+
+    public static int rollTheDice() {
+        randomNumber = new Random();
+        return number01 = randomNumber.nextInt(1, 9);
+    }
+
+    public static int sumOfPickedNumbers(int number01, int number02, int number03) {
+        return number01+number02+number03;
+    }
+
+    public static void compareResults(int result01, int result02){
+        if (result01 > result02){
+            System.out.println("Player with sum of the results: " + result01 + ", WIN!!!");
+        }
+        else if (result01 < result02) {
+            System.out.println("Player with sum of the results: " + result02 + ", WIN!!!");
+        }
+        else
+            System.out.println("Results are the same!!!");
+
+    }
+}
--- src/com/capgemini/kakuta/Main.java
+++ src/com/capgemini/kakuta/Main.java
@@ -1,9 +1,26 @@
 package com.capgemini.kakuta;
 
+import com.capgemini.kakuta.DiceJack.RollTheDice;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(ReturnValues.calculateArea(2.0, 3.5));
-        System.out.println(ReturnValues.explainArea("English"));
+        int userNumber1 = RollTheDice.rollTheDice();
+        int userNumber2 = RollTheDice.rollTheDice();
+        int userNumber3 = RollTheDice.rollTheDice();
+
+        int userSum = RollTheDice.sumOfPickedNumbers(userNumber1, userNumber2, userNumber3);
+
+        int mineNumber1 = RollTheDice.rollTheDice();
+        int mineNumber2 = RollTheDice.rollTheDice();
+        int mineNumber3 = RollTheDice.rollTheDice();
+
+        int mineSum = RollTheDice.sumOfPickedNumbers(mineNumber1, mineNumber2, mineNumber3);
+
+
+        System.out.println("User picked up a sum of: " + userSum);
+        System.out.println("I picked up a sum of: " + mineSum);
+
+        RollTheDice.compareResults(userSum, mineSum);
     }
 }
