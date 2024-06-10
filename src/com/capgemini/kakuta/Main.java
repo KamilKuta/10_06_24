d3705a9face3997c919313173c18d8031912ea40
Kamil Kuta <kamil.kuta@capgemini.com>
2024-06-10 15:17:34 +0200

    Clean up the main class

--- src/com/capgemini/kakuta/Main.java
+++ src/com/capgemini/kakuta/Main.java
@@ -3,7 +3,5 @@ package com.capgemini.kakuta;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(ReturnValues.calculateArea(2.0, 3.5));
-        System.out.println(ReturnValues.explainArea("English"));
     }
 }
