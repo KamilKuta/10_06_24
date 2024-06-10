0a29849058b57c74b755c1812e31617153a14c62
Kamil Kuta <kamil.kuta@capgemini.com>
2024-06-10 16:47:09 +0200

    implementation for DiceJack game

--- src/com/capgemini/kakuta/Main.java
+++ src/com/capgemini/kakuta/Main.java
@@ -1,7 +1,26 @@
 package com.capgemini.kakuta;
 
+import com.capgemini.kakuta.DiceJack.RollTheDice;
+
 public class Main {
 
     public static void main(String[] args) {
+        int userNumber1 = RollTheDice.rollTheDice();
+        int userNumber2 = RollTheDice.rollTheDice();
+        int userNumber3 = RollTheDice.rollTheDice();
+
+        int userSum = RollTheDice.sumOfPickedNumbers(userNumber1, userNumber2, userNumber3);
+
+        int mineNumber1 = RollTheDice.rollTheDice();
+        int mineNumber2 = RollTheDice.rollTheDice();
+        int mineNumber3 = RollTheDice.rollTheDice();
+
+        int mineSum = RollTheDice.sumOfPickedNumbers(mineNumber1, mineNumber2, mineNumber3);
+
+
+        System.out.println("User picked up a sum of: " + userSum);
+        System.out.println("I picked up a sum of: " + mineSum);
+
+        RollTheDice.compareResults(userSum, mineSum);
     }
 }
