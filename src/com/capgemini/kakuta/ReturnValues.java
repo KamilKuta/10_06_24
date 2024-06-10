9a718f1038e889e4cb7b61498d7be7294b2ee2de
Kamil Kuta <kamil.kuta@capgemini.com>
2024-06-10 12:05:18 +0200

    added explainArea method

--- src/com/capgemini/kakuta/ReturnValues.java
+++ src/com/capgemini/kakuta/ReturnValues.java
@@ -2,4 +2,17 @@ package com.capgemini.kakuta;
 
 public class ReturnValues {
 
+    public static double calculateArea(double length, double width){
+        double area = length + width;
+        return area;
+    }
+
+    public static String explainArea(String language){
+     switch(language){
+         case "English": return "Area equals length * width";
+         case "French": return "La surface est egale a la longueur * la largeur";
+         case "Spanish": return "area es igual a largo * ancho";
+         default: return "Language not available";
+     }
+    }
 }
