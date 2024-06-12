02592579e5be4f357d8015e27bb4169f380d3d53
Kamil Kuta <kamil.kuta@capgemini.com>
2024-06-12 14:47:50 +0200

    practices for Arrays

---
+++ src/com/capgemini/kakuta/Arrays/IntroToArrays.java
@@ -0,0 +1,12 @@
+package com.capgemini.kakuta.Arrays;
+
+public class IntroToArrays {
+    public static void main(String[] args) {
+        String kingdom1 = "Mercia";
+        String kingdom2 = "Wessex";
+        String kingdom3 = "Northumbria";
+        String kingdom4 = "East Anglia";
+
+        String[] kingdoms = {"Mercia", "Wessex", "Northumbria", "East Anglia" };
+    }
+}
