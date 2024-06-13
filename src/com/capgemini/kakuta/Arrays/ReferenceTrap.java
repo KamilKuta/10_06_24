4d690f90d29e5f22f04456a9a6e2d23e768da315
Kamil Kuta <kamil.kuta@capgemini.com>
2024-06-13 12:04:18 +0200

    Reference trap exercise for array lists

---
+++ src/com/capgemini/kakuta/Arrays/ReferenceTrap.java
@@ -0,0 +1,30 @@
+package com.capgemini.kakuta.Arrays;
+
+import java.util.Arrays;
+
+public class ReferenceTrap {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3};
+        int[] numbers2 = numbers;
+
+        numbers2[1] = 5;
+        System.out.println(Arrays.toString(numbers));
+        System.out.println(Arrays.toString(numbers2));
+
+        String[] staffLastYear = {"Tommy", "Joel", "Ellie"};
+        String[] staffThisYear = staffLastYear;
+        staffThisYear[1] = "Abby";
+
+        System.out.println(Arrays.toString(staffLastYear));
+        System.out.println(Arrays.toString(staffThisYear));
+
+        //Solution to avoid reference TRAP is to create a new empty list and copy/paste old list into the new one by for loop
+        String[] staff = {"Tommy", "Joel", "Ellie"};
+        String[] newStaff = new String[5];
+
+        for (int i = 0; i < staff.length; i++) {
+            newStaff[i] = staff[i];
+        }
+        System.out.println(Arrays.toString(newStaff));
+    }
+}
